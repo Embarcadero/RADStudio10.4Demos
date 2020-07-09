046a2caa752c5dcf726f3b2b15f92eab3742603b
Horácio Filho <horacio.filho@embarcadero.com>
2020-07-09 18:42:28 -0300

    Syncing the internal and public repositories.

--- CPP/Mobile Snippets/Notifications/SendCancelNotification/uMain.cpp
+++ CPP/Mobile Snippets/Notifications/SendCancelNotification/uMain.cpp
@@ -6,96 +6,83 @@
 // are an authorized licensee of Delphi, C++Builder or RAD Studio 
 // (the "Embarcadero Products").  This software is subject to Embarcadero's 
 // standard software license and support agreement that accompanied your 
-// purchase of the Embarcadero Products and is considered a Redistributable, 
-// as such term is defined thereunder. Your use of this software constitutes 
-// your acknowledgement of your agreement to the foregoing software license 
-// and support agreement. 
+// purchase of the Embarcadero Products and is considered a Redistributable,
+// as such term is defined thereunder. Your use of this software constitutes
+// your acknowledgement of your agreement to the foregoing software license
+// and support agreement.
 //---------------------------------------------------------------------------
 
 #include <fmx.h>
-#include <System.DateUtils.hpp>
+#include <memory>
 #pragma hdrstop
 
 #include "uMain.h"
 // ---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.fmx"
-#pragma resource ("*.LgXhdpiPh.fmx", _PLAT_ANDROID)
-
-#include <FMX.Dialogs.hpp>
-/*#include <FMX.DialogService.Async.hpp>
-#ifndef _PLAT_ANDROID
-#include <FMX.DialogService.Sync.hpp>
-#endif*/
 
 TNotificationsForm *NotificationsForm;
 
 // ---------------------------------------------------------------------------
-__fastcall TNotificationsForm::TNotificationsForm(TComponent* Owner)
-	: TForm(Owner) {
+__fastcall TNotificationsForm::TNotificationsForm(TComponent *Owner) : TForm(Owner) {
 }
-
 // ---------------------------------------------------------------------------
-void __fastcall TNotificationsForm::btnSendScheduledNotificationClick
-	(TObject *Sender)
-
+void __fastcall TNotificationsForm::NotificationCPermissionRequestResult(TObject *Sender, const bool AIsGranted)
 {
-	// verify if the service is actually supported
-	TNotification * Notification = NotificationC->CreateNotification();
+	if (AIsGranted && PendingAction)
+	{
+		PendingAction->Execute();
+	}
 
+	PendingAction = nullptr;
+}
+// ---------------------------------------------------------------------------
+void __fastcall TNotificationsForm::NotificationCReceiveLocalNotification(TObject *Sender, TNotification *ANotification)
+{
+	Memo1->Lines->Add(ANotification->AlertBody);
+}
+// ---------------------------------------------------------------------------
+void __fastcall TNotificationsForm::ActionListExecute(TBasicAction *Action, bool &Handled)
+{
+	if (NotificationC->AuthorizationStatus() != TAuthorizationStatus::Authorized)
+	{
+		Handled = true;
+		PendingAction = Action;
+
+		NotificationC->RequestPermission();
+	}
+}
+// ---------------------------------------------------------------------------
+void __fastcall TNotificationsForm::ActionSendScheduledNotificationExecute(TObject *Sender)
+{
+	std::unique_ptr<TNotification> Notification(NotificationC->CreateNotification());
 	Notification->Name = "MyNotification";
 	Notification->AlertBody = "C++Builder for Mobile is here!";
-
 	/* Fired in 10 second */
-	Notification->FireDate = System::Dateutils::IncSecond(Now(), 10);
+	Notification->FireDate = Now() + EncodeTime(0, 0, 10, 0);
 
 	/* Send notification in Notification Center */
-	NotificationC->ScheduleNotification(Notification);
-
-	/* Free the memory */
-	Notification->DisposeOf();
+	NotificationC->ScheduleNotification(Notification.get());
 }
 // ---------------------------------------------------------------------------
-
-void __fastcall TNotificationsForm::btnSendNotificationImmediatelyClick(TObject *Sender)
-
+void __fastcall TNotificationsForm::ActionSendNotificationImmediatelyExecute(TObject *Sender)
 {
-	// verify if the service is actually supported
-	TNotification * Notification = NotificationC->CreateNotification();
-
-	Notification->Name = "MyNotification";
+	std::unique_ptr<TNotification> Notification(NotificationC->CreateNotification());
 	Notification->AlertBody = "C++Builder for Mobile is here!";
 	Notification->FireDate = Now();
 
 	// Send notification in Notification Center
-	NotificationC->PresentNotification(Notification);
-
-	// also this method is equivalent if supported
-	//NotificationC->ScheduleNotification(Notification);
-
-	// Free the notification
-	Notification->DisposeOf();
+	NotificationC->PresentNotification(Notification.get());
 }
-//---------------------------------------------------------------------------
-
-void __fastcall TNotificationsForm::SpeedButton1Click(TObject *Sender)
+// ---------------------------------------------------------------------------
+void __fastcall TNotificationsForm::ActionCancelScheduledExecute(TObject *Sender)
 {
-		// providing the fact that you already have a MyNotification previously issued
+	// Providing the fact that you already have a MyNotification previously issued
 	NotificationC->CancelNotification("MyNotification");
 }
-//---------------------------------------------------------------------------
-
-void __fastcall TNotificationsForm::SpeedButton2Click(TObject *Sender)
+// ---------------------------------------------------------------------------
+void __fastcall TNotificationsForm::ActionCancelAllNotificationsExecute(TObject *Sender)
 {
 	NotificationC->CancelAll();
 }
-
-//---------------------------------------------------------------------------
-void __fastcall TNotificationsForm::NotificationCReceiveLocalNotification(TObject *Sender,
-		  TNotification *ANotification)
-{
-	if(ANotification)
-		Memo1->Lines->Add(ANotification->AlertBody);
-}
-//---------------------------------------------------------------------------
-
+// ---------------------------------------------------------------------------
--- CPP/Mobile Snippets/Notifications/SendCancelNotification/uMain.h
+++ CPP/Mobile Snippets/Notifications/SendCancelNotification/uMain.h
@@ -12,17 +12,19 @@
 #ifndef uMainH
 #define uMainH
 //---------------------------------------------------------------------------
+
+#include <System.Actions.hpp>
 #include <System.Classes.hpp>
+#include <System.Notification.hpp>
+#include <FMX.ActnList.hpp>
 #include <FMX.Controls.hpp>
+#include <FMX.Controls.Presentation.hpp>
 #include <FMX.Forms.hpp>
+#include <FMX.Memo.hpp>
+#include <FMX.Memo.Types.hpp>
+#include <FMX.ScrollBox.hpp>
 #include <FMX.StdCtrls.hpp>
 #include <FMX.Types.hpp>
-#include <System.Notification.hpp>
-#include <FMX.MobilePreview.hpp>
-#include <FMX.Controls.Presentation.hpp>
-#include <System.Notification.hpp>
-#include <FMX.Memo.hpp>
-#include <FMX.ScrollBox.hpp>
 //---------------------------------------------------------------------------
 class TNotificationsForm : public TForm
 {
@@ -36,15 +38,23 @@ __published:	// IDE-managed Components
 	TSpeedButton *SpeedButton2;
 	TNotificationCenter *NotificationC;
 	TMemo *Memo1;
-	void __fastcall btnSendScheduledNotificationClick(TObject *Sender);
-	void __fastcall btnSendNotificationImmediatelyClick(TObject *Sender);
-	void __fastcall SpeedButton1Click(TObject *Sender);
-	void __fastcall SpeedButton2Click(TObject *Sender);
-	void __fastcall NotificationCReceiveLocalNotification(TObject *Sender, TNotification *ANotification);
+	TActionList *ActionList;
+	TAction *ActionSendScheduledNotification;
+	TAction *ActionSendNotificationImmediately;
+	TAction *ActionCancelScheduled;
+	TAction *ActionCancelAllNotifications;
 
+	void __fastcall NotificationCPermissionRequestResult(TObject *Sender, const bool AIsGranted);
+	void __fastcall NotificationCReceiveLocalNotification(TObject *Sender, TNotification *ANotification);
+	void __fastcall ActionSendScheduledNotificationExecute(TObject *Sender);
+	void __fastcall ActionSendNotificationImmediatelyExecute(TObject *Sender);
+	void __fastcall ActionCancelScheduledExecute(TObject *Sender);
+	void __fastcall ActionCancelAllNotificationsExecute(TObject *Sender);
+	void __fastcall ActionListExecute(TBasicAction *Action, bool &Handled);
 private:	// User declarations
+	TBasicAction *PendingAction;
 public:		// User declarations
-	__fastcall TNotificationsForm(TComponent* Owner);
+	__fastcall TNotificationsForm(TComponent *Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TNotificationsForm *NotificationsForm;
